f9b01b57e2f76a09c6ae5992a8b88b1e87dd4eeb
Eeshan709 <eeshan.edu@gmail.com>
2023-08-28 08:29:30 +0000

    commit1

--- financial-service/src/main/java/com/tech_tribe/financialservice/utility/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.tech_tribe.financialservice.utility;
-
-public enum UserRole {
-    User,Financial_Advisor,Admin
-}
--- plan-generator-service/src/main/java/com/tech_tribe/plangeneratorservice/utility/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.tech_tribe.plangeneratorservice.utility;
-
-public enum UserRole {
-    User,Financial_Advisor,Admin
-}
